12a1d8d8b8daa1d596a47195506e519645f0d187
hitoshidevx <g.hitoshi.yokogawa@gmail.com>
2023-10-05 10:08:50 -0300

    Adicionando Link para Review na Home

--- rankdaty/src/app/page.js
+++ rankdaty/src/app/page.js
@@ -1,6 +1,7 @@
 import NavBar from '@/components/NavBar'
 import Image from 'next/image'
 import { ChatBubbleOvalLeftEllipsisIcon } from '@heroicons/react/24/solid'
+import ButtonLink from '@/components/ButtonLink'
 
 
 export default function Home() {
@@ -9,8 +10,8 @@ export default function Home() {
       <NavBar active={"reviews"} />
 
       <main className='bg-indigo-950 p-11 m-10 rounded-lg'>
-        <h2 className='text-2x1 font-bold'>Dashboard</h2>
-
+        <h2 className='text-2x1 font-bold mb-10'>Clique no botão abaixo!</h2>
+        <ButtonLink variante='secundary' href='/opiniao'>Minhas Reviews</ButtonLink>
       </main>
     </>
   )
